59310aebe4df759c5336392725a4a7ad38cda6bb
dolphin <fun53@ukr.net>
2023-09-03 20:02:48 +0300

    comp

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,57 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+import { Component } from "react";
+import { Searchbar } from "./Searchbar/Searchbar";
+import { serviceGetImages } from "api";
+import { Loader } from "./Loader/Loader";
+import { ImageGallery } from "./ImageGallery/ImageGallery";
+
+export class App extends Component {
+  state = {
+    gallery: [],
+    query: {
+      searchString: '',
+      page: 1,
+      timeStamp: null,
+    },
+    loader: false,
+    error: false,
+  }
+
+  async componentDidUpdate(prevProps, prevState){
+    if (prevState.query.timeStamp !== null && prevState.query.timeStamp !== this.state.query.timeStamp){
+      try {
+        this.setState({loader: true, error: false});
+        const gallery = await serviceGetImages(this.state.query);
+        this.setState({gallery})
+      } catch (error) {
+        this.setState({ error: true });
+      } finally {
+        this.setState({ loader: false });        
+      }
+    }
+  }
+
+  
+  handleChange = (ev) => {
+    // console.log(ev.target.value);
+    this.setState({query: {searchString: ev.target.value}})
+  }
+
+  handleSubmit = (ev) =>{
+    ev.preventDefault();
+    // ()=>{prevState=>console.log('prevState= ', prevState)}()
+    console.log('form= ', ev.target.search.value)
+    this.setState({query: {searchString: ev.target.search.value, page: 1, timeStamp: Date.now()}});
+    console.log(this.state)
+  }
+
+
+  render (){
+    return(
+      <div>
+      <Searchbar search={this.state.query.searchString} onChange={this.handleChange} onSubmit={this.handleSubmit}/>
+      {this.state.loader && <Loader/>}
+      {this.state.gallery.length>0 && <ImageGallery gallery={this.state.gallery}/>}
     </div>
-  );
+    )
+  };
 };
---
+++ src/components/ImageGallery/ImageGallery.js
@@ -0,0 +1,10 @@
+import { ImageGalleryItem } from "components/ImageGalleryItem/ImageGalleryItem"
+import { Gallery } from "./ImageGallery.styled"
+
+export const ImageGallery = ({gallery}) =>{
+   return(
+      <Gallery>
+         {gallery.map(el=><ImageGalleryItem key={el.id} url={el.webformatURL} tags={el.tags}/>)}
+      </Gallery>
+   )
+}
